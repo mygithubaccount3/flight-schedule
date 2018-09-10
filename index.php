5be314a43308c5936ad789bcedfba766b4332cfb
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2018-09-10 15:12:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,7 @@
         <form>
             <input type='text' id='inp' placeholder='Search: type destination'>
         </form>
-        <div id = 'time'>
-        </div>
-        <script type="text/javascript"></script>
-        <div id="dv">
+        <div id = 'time'></div>
         <table id="tbl">
             <tr>
                 <th>Flight</th>
@@ -38,22 +35,15 @@
         $result = $conn->query($qr);
         $rows_count = $result->fetch_array(MYSQLI_NUM);
         $accRow = uniqRand(0, $rows_count[0], 5);
-        echo count($accRow);
         for ($i=0; $i < count($accRow); $i++) { 
            $accElLength = rand(0, $n - 1);
             $schedule_event = "CREATE EVENT IF NOT EXISTS random_status_$i ON SCHEDULE EVERY 1 DAY STARTS CURRENT_TIMESTAMP DO UPDATE flights SET status='$statuses[$accElLength]' WHERE id='$accRow[$i]'";
-            //echo $schedule_event."<br>";
-
             $conn->query($schedule_event);
         }
-        
         $query = "SELECT * FROM flights ORDER BY departure_time";
-        //$query = "UPDATE flights SET departure_time='23:15:00' WHERE destination='Kyiv'";
-        //$conn->query($query);
         $result = $conn->query($query);
         $row = "";
         if (!result) die("Fatal error");
-        $n = 0;
     while($row = $result->fetch_assoc()) {
         echo "<tr>" . "<td>". $row["flight"] . "</td>" . "<td>" . $row["departure_time"] . "</td>" . "<td>" . $row["destination"] . "</td>" . "<td>" . $row["carrier"] . "</td>" . "<td>" . $row["terminal"] . "</td>" . "<td>" . $row["status"] . "</td>" . "</tr>";
     }
